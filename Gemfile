eeb36e1b1d546b036bb1e3645e328b6b9fb18d3c
alex <alekskushn@gmail.com>
2016-01-28 12:28:56 +0200

    seed db, fix user model

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'pg'
 gem 'puma'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :password,  length: { minimum: 6 }
+  validates :password,  length: { minimum: 5 }
 end
