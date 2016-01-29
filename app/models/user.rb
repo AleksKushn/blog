a4f8d6e161cfc94af2ff36675e9143072bc0a9a4
alex <alekskushn@gmail.com>
2016-01-29 13:33:32 +0200

    add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,36 @@ class User < ApplicationRecord
   has_many :comments, :foreign_key => :author_id
 
   has_secure_password
+  before_create :set_initial_api_key
   before_save { email.downcase! }
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :password,  length: { minimum: 5 }
+  validates :api_key, uniqueness: true
+
+
+  def as_json(options = {})
+    {
+        :id            => id,
+        :name          => name,
+        :email         => email,
+        :created_at    => created_at,
+        :updated_at    => updated_at
+    }
+  end
+
+  private
+
+  def set_initial_api_key
+    self.api_key ||= generate_api_key
+  end
+
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      break token unless User.exists?(api_key: token)
+    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128123245) do
+ActiveRecord::Schema.define(version: 20160129112626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160128123245) do
     t.string   "api_key",         default: "", null: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
